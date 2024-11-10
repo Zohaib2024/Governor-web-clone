bce1337af14985db1d12ad7d3d525d2b8756ffdc
Zohaib <azohaibshoukat22@gmail.com>
2024-11-11 00:29:01 +0500

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-import Navbar from "./components/Navbar";
+
+
 import Hero from "./components/Hero";
 import Info from "./components/Info";
 import Corecol from "./components/Corecol";
-import Footer from "./components/Footer";
+
 import Advcol from "./components/Advcol";
 
 
